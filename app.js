a833fde24e11d34cd3e7b4d310669bc48b51b2eb
Thibaud Colas <thibaudcolas@gmail.com>
2013-09-21 14:48:31 +0200

    API now works as intended

--- app.js
+++ app.js
@@ -17,19 +17,24 @@ db.once('open', function callback () {
   });
 });
 
+/**
+ * Reproduces the data model from the SOAP WS w/ additional information.
+ */
 var stationSchema = mongoose.Schema({
     ID: String,
     Name: String,
     City: String,
     CB: Boolean,
+    SlotsTotal : Number,
+    BikesAvailable: Number,
+    Lat: Number,
+    Lng: Number,
+    SlotsAvailable: Number,
     Updated: {
       type: Date,
       default: Date.now
     },
-    SlotsTotal : Number,
-    BikesAvailable: Number,
-    SlotsAvailable: Number,
-    Coordinates: {
+    Geo: {
       type: [Number],
       index: '2d'
     }
@@ -38,22 +43,11 @@ var stationSchema = mongoose.Schema({
 var Station = mongoose.model('Station', stationSchema);
 
 /*
-{
-  "ID": 1,
-  "Name": "Gare Saint-Roch",
-  "City": "Montpellier",
-  "Lat": 43.605313,
-  "Lng": 3.880934,
-  "CB": 1,
-  "SlotsTotal": 24,
-  "BikesAvailable": 16,
-  "SlotsAvailable": 8
-},
 LAT/LNG
 43.61092/3.87723
 **/
 /**
- * Vélos au départ
+    Vélos au départ
     Idem à l'arrivée
 
     Stations selon coord + perim
@@ -61,26 +55,82 @@ LAT/LNG
 
 
     Station la plus proche qui a des places
-    Station la plus proche qui a des place
+    Station la plus proche qui a des vélos
  */
 
 var app = express();
+
+var conf = {
+  maxResults: 3
+};
 
 var retrieveAllStations = function (callback) {
   rest.get('http://192.168.1.148/Bikes/Stations', {parser: rest.parsers.json}).on('complete', callback);
 };
 
-app.get('/near/:lat/:lon', function (req, resp) {
-  var loc = {$geometry :  {type : "Point" , coordinates: [parseFloat(req.params.lon), parseFloat(req.params.lat)]} };
-  Station.find({Coordinates: {
-    $near: [parseFloat(req.params.lon), parseFloat(req.params.lat)]
-  }}).exec(function (error, stations) {
-    if (error) {
-      resp.send(error);
+var stationMapping = function (raw) {
+  return new Station({
+    ID: raw.ID,
+    Name: raw.Name,
+    City: raw.City,
+    CB: raw.CB === 1,
+    SlotsTotal : raw.SlotsTotal,
+    BikesAvailable: raw.BikesAvailable,
+    SlotsAvailable: raw.SlotsAvailable,
+    Lat: parseFloat(raw.Lat),
+    Lng: parseFloat(raw.Lng),
+    Geo: [
+      parseFloat(raw.Lng),
+      parseFloat(raw.Lat)
+    ]
+  });
+};
+
+app.get('/bikes/:lat/:lon/:threshold', function (req, resp) {
+  var loc = [parseFloat(req.params.lon), parseFloat(req.params.lat)];
+  Station.find({
+    Geo: {
+      $near: loc
+    },
+    BikesAvailable: {
+      $gte: req.params.threshold
     }
-    else {
-      resp.send(stations);
+  }).limit(conf.maxResults).exec(function (error, stations) {
+    console.log(stations[0]);
+    resp.send(stations);
+  });
+});
+
+app.get('/slots/:lat/:lon/:threshold', function (req, resp) {
+  var loc = [parseFloat(req.params.lon), parseFloat(req.params.lat)];
+  Station.find({
+    Geo: {
+      $near: loc
+    },
+    SlotsAvailable: {
+      $gte: req.params.threshold
     }
+  }).limit(conf.maxResults).exec(function (error, stations) {
+    console.log(stations[0]);
+    resp.send(stations);
+  });
+});
+
+app.get('/stations/:lat/:lon/:threshold', function (req, resp) {
+  var loc = [parseFloat(req.params.lon), parseFloat(req.params.lat)];
+  Station.find({
+    Geo: {
+      $near: loc
+    },
+    BikesAvailable: {
+      $gte: req.params.threshold
+    },
+    SlotsAvailable: {
+      $gte: req.params.threshold
+    }
+  }).limit(conf.maxResults).exec(function (error, stations) {
+    console.log(stations[0]);
+    resp.send(stations);
   });
 });
 
@@ -90,30 +140,15 @@ app.get('/update', function (req, resp) {
     if (err) {
       console.log('Error removing old data');
     }
-    else {
-      console.log('Removed old data')
-    }
   });
 
   retrieveAllStations(function (stations) {
     var s;
     stations.forEach(function (e, i, array) {
-      s = new Station({
-        ID: e.ID,
-        Name: e.Name,
-        City: e.City,
-        CB: e.CB === 1,
-        SlotsTotal : e.SlotsTotal,
-        BikesAvailable: e.BikesAvailable,
-        SlotsAvailable: e.SlotsAvailable,
-        Coordinates: [parseFloat(e.Lng), parseFloat(e.Lat)]
-      });
+      s = stationMapping(e);
       s.save(function (err, s) {
         if (err) {
           console.log(err);
-        }
-        else {
-          console.log('Stored ' + s.ID);
         }
       });
     });
